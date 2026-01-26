582eef19dc0bf11b3c7c3459b139f7d4debbb843
nonyx777 <nahomretebo@gmail.com>
2026-01-26 16:09:30 +0300

    Optimized Polygonize function

--- Scripts/MarchinCube.gd
+++ Scripts/MarchinCube.gd
@@ -5,15 +5,17 @@ var isolevel: float = 2
 # grid
 var grid_size: Vector3i = Vector3i(10, 10, 10)
 var spacing: float = 1
-var points: PackedVector3Array = []
-var gridcells: Array = []
-var triangles: Array = []
+# grid cell related
+var grid_pos: PackedVector3Array
+var grid_norm: PackedVector3Array
+var grid_val: PackedFloat32Array
+# triangle related
+var tri_pos: PackedVector3Array
+var tri_norm: PackedVector3Array
 
 # surface
 var surface_array = Array()
-var verts: PackedVector3Array = PackedVector3Array()
 var indices: PackedInt32Array = PackedInt32Array()
-var normals: PackedVector3Array = PackedVector3Array()
 
 @onready var meshInstance: MeshInstance3D = $MeshInstance3D
 
@@ -21,20 +23,17 @@ func sphere_sdf(p: Vector3, r: float):
 	return (p - Vector3(5, 5, 5)).length() - r
 
 func generate_points(sx: int, sy: int, sz: int, spacing_: float):
-	points.clear()
-	for z in range(sz + 1):
-		for y in range(sy + 1):
-			for x in range(sx + 1):
-				points.append(Vector3(x * spacing_, y * spacing_, z * spacing_))
+	grid_pos.clear()
+	for z in range(sz):
+		for y in range(sy):
+			for x in range(sx):
+				grid_pos.append(Vector3(x * spacing_, y * spacing_, z * spacing_))
 
 func get_point_index(x: int, y: int, z: int, sx: int, sy: int):
 	return z * (sy + 1) * (sx + 1) + y * (sx + 1) + x
 
-func get_cell(x, y, z) -> Polygonise.GRIDCELL:
-	var sx = grid_size.x
-	var sy = grid_size.y
-	var sz = grid_size.z
-	var i0 = get_point_index(x, y, z, sx, sy)
+func setup_cells(i: int, sx: int, sy: int, sz: int) -> void:
+	var i0 = i
 	var i1 = i0 + 1
 	var i2 = i0 + (sx + 1)
 	var i3 = i2 + 1
@@ -43,30 +42,38 @@ func get_cell(x, y, z) -> Polygonise.GRIDCELL:
 	var i6 = i4 + (sx + 1)
 	var i7 = i6 + 1
 	
-	var grid: Polygonise.GRIDCELL = Polygonise.GRIDCELL.new()
-	grid.p = PackedVector3Array([points[i0], points[i1], points[i3], points[i2], points[i4], points[i5], points[i7], points[i6]])
-	grid.val = PackedFloat32Array([sphere_sdf(points[i0], isolevel), sphere_sdf(points[i1], isolevel), sphere_sdf(points[i3], isolevel), sphere_sdf(points[i2], isolevel), sphere_sdf(points[i4], isolevel), sphere_sdf(points[i5], isolevel), sphere_sdf(points[i7], isolevel), sphere_sdf(points[i6], isolevel)])
-	grid.norm = PackedVector3Array()
-	grid.norm.resize(grid.p.size())
+	if i7 >= (sx * sx * sx):
+		return
 	
-	# Computer normal using centeral difference
-	for i in range(grid.p.size()):
-		var g = grid.p[i]
-		var g_x = sphere_sdf(Vector3(g.x + 1, g.y, g.z) - Vector3(g.x-1, g.y, g.z), isolevel) / sx
-		var g_y = sphere_sdf(Vector3(g.x, g.y + 1, g.z) - Vector3(g.x, g.y-1, g.z), isolevel) / sy
-		var g_z = sphere_sdf(Vector3(g.x, g.y, g.z + 1) - Vector3(g.x, g.y, g.z - 1), isolevel) / sz
-		grid.norm[i] = Vector3(g_x, g_y, g_z)
+	grid_val[i0] = sphere_sdf(grid_pos[i0], isolevel)
+	grid_val[i1] = sphere_sdf(grid_pos[i1], isolevel)
+	grid_val[i2] = sphere_sdf(grid_pos[i2], isolevel)
+	grid_val[i3] = sphere_sdf(grid_pos[i3], isolevel)
+	grid_val[i4] = sphere_sdf(grid_pos[i4], isolevel)
+	grid_val[i5] = sphere_sdf(grid_pos[i5], isolevel)
+	grid_val[i6] = sphere_sdf(grid_pos[i6], isolevel)
+	grid_val[i7] = sphere_sdf(grid_pos[i7], isolevel)
 	
-	return grid
+	# Computer normal using central difference
+	for idx in [i0, i1, i2, i3, i4, i5, i6, i7]:
+		var pos: Vector3 = grid_pos[idx]
+		var n_x = sphere_sdf(Vector3(pos.x + 1, pos.y, pos.z) - Vector3(pos.x-1, pos.y, pos.z), isolevel) / sx
+		var n_y = sphere_sdf(Vector3(pos.x, pos.y + 1, pos.z) - Vector3(pos.x, pos.y-1, pos.z), isolevel) / sy
+		var n_z = sphere_sdf(Vector3(pos.x, pos.y, pos.z + 1) - Vector3(pos.x, pos.y, pos.z - 1), isolevel) / sz
+		grid_norm[idx] = Vector3(n_x, n_y, n_z)
 
 func construct_grid() -> void:
 	generate_points(grid_size.x, grid_size.y, grid_size.z, spacing)
-	for z in range(grid_size.z):
-		for y in range(grid_size.y):
-			for x in range(grid_size.x):
-				gridcells.append(get_cell(x, y, z))
+	grid_val.clear()
+	grid_norm.clear()
+	grid_val.resize(grid_pos.size())
+	grid_norm.resize(grid_pos.size())
+	for i in range(grid_pos.size()):
+		setup_cells(i, grid_size.x, grid_size.y, grid_size.z)
 
 func main_march() -> void:
+	tri_pos.clear()
+	tri_norm.clear()
 	# initialize surface array
 	meshInstance.mesh.clear_surfaces()
 	surface_array.resize(Mesh.ARRAY_MAX)
@@ -74,30 +81,18 @@ func main_march() -> void:
 	surface_array[Mesh.ARRAY_INDEX] = PackedInt32Array()
 	surface_array[Mesh.ARRAY_NORMAL] = PackedVector3Array()
 	
-	verts.clear()
 	indices.clear()
-	normals.clear()
 	
 	var polygonize: Polygonise = Polygonise.new()
-	triangles.resize(5)
+	var n = polygonize.Polygonize(grid_pos, grid_norm, grid_val, isolevel, grid_size.x, grid_size.y, tri_pos, tri_norm)
 	
-	var n = 0
-	for cell in gridcells:
-		var ntri: int = 0
-		ntri = polygonize.Polygonize(cell, isolevel, triangles)
-		n += ntri
-		for i in range(ntri):
-			for vert in triangles[i].p:
-				verts.append(vert)
-			for norm in triangles[i].n:
-				normals.append(norm)
 	
 	for i in range(n*3):
 		indices.append(i)
 	
-	surface_array[Mesh.ARRAY_VERTEX] = verts
+	surface_array[Mesh.ARRAY_VERTEX] = tri_pos
 	surface_array[Mesh.ARRAY_INDEX] = indices
-	surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_NORMAL] = tri_norm
 	meshInstance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
 	
 	# Sphere instances for testing
@@ -120,12 +115,11 @@ func main_march() -> void:
 	#add_child(multi_mesh_instance)
 	
 	
-	print("Points: ", points.size())
-	print("GridCells: ", gridcells.size())
+	print("Points: ", grid_pos.size())
 	print("Number of Triangles: ", n)
-	print("Number of Vertices: ", verts.size())
+	print("Number of Vertices: ", tri_pos.size())
 	print("Number of Indices: ", indices.size())
-	print("Number of Normals: ", normals.size())
+	print("Number of Normals: ", tri_norm.size())
 
 func _ready() -> void:
 	construct_grid()
--- Scripts/Polygonise.gd
+++ Scripts/Polygonise.gd
@@ -1,6 +1,8 @@
 class_name Polygonise
 extends Node
 var lookup = Lookup.new()
+var edgeTable = lookup.edgeTables
+var triTable = lookup.triTable
 
 func VertexInterp(isolevel: float, p1: Vector3, p2: Vector3, valp1: float, valp2: float) -> Vector3:
 	var mu: float
@@ -16,117 +18,104 @@ func VertexInterp(isolevel: float, p1: Vector3, p2: Vector3, valp1: float, valp2
 	p = p1 + mu * (p2 - p1)
 	return p
 
-# Data orientize
-# triangle
-var tri_p: PackedVector3Array
-var tri_n: PackedVector3Array
-# GridCell
-var gc_p: PackedVector3Array
-var gc_val: PackedVector3Array
-var gc_norm: PackedVector3Array
-
-class TRIANGLE:
-	var p: PackedVector3Array
-	var n: PackedVector3Array
-
-class GRIDCELL:
-	var p: PackedVector3Array
-	var val: PackedFloat32Array
-	var norm: PackedVector3Array
-
 # polygonize function should work on all the array instead of processing a single gridcell at a time
 # it should take in p, val, and norm separately
-func Polygonize(grid: GRIDCELL, isolevel: float, triangles) -> int:
+func Polygonize(gc_pos: PackedVector3Array, gc_norm: PackedVector3Array, gc_val: PackedFloat32Array, isolevel: float, sx: int, sy:int, tri_pos: PackedVector3Array, tri_norm: PackedVector3Array) -> int:
 	var i: int
-	var ntriang: int
-	var cubeindex: int
+	var ntriang: int = 0
+	
 	var vertlist: PackedVector3Array
 	var normlist: PackedVector3Array
 	vertlist.resize(12)
 	normlist.resize(12)
-	
 	# after this everything should happen in a forloop
 	# this means every neighbouring element should be computed everytime
+	for idx in range(gc_pos.size()):
+		# Calculate neighbours
+		var i0: int = idx
+		var i1: int = i0 + 1
+		var i2: int = i0 + (sx + 1)
+		var i3: int = i2 + 1
+		var i4: int = i0 + (sy + 1) * (sx + 1)
+		var i5: int = i4 + 1
+		var i6: int = i4 + (sx + 1)
+		var i7: int = i6 + 1
+		
+		if i7 >= (sx * sx * sx):
+			continue
+		
+		#Determine the index into the edge table
+		var cubeindex: int = 0
+		if gc_val[i0] < isolevel:
+			cubeindex |= 1
+		if gc_val[i1] < isolevel:
+			cubeindex |= 2
+		if gc_val[i2] < isolevel:
+			cubeindex |= 4
+		if gc_val[i3] < isolevel:
+			cubeindex |= 8
+		if gc_val[i4] < isolevel:
+			cubeindex |= 16
+		if gc_val[i5] < isolevel:
+			cubeindex |= 32
+		if gc_val[i6] < isolevel:
+			cubeindex |= 64
+		if gc_val[i7] < isolevel:
+			cubeindex |= 128
 	
-	#Determine the index into the edge table
-	cubeindex = 0
-	if grid.val[0] < isolevel:
-		cubeindex |= 1
-	if grid.val[1] < isolevel:
-		cubeindex |= 2
-	if grid.val[2] < isolevel:
-		cubeindex |= 4
-	if grid.val[3] < isolevel:
-		cubeindex |= 8
-	if grid.val[4] < isolevel:
-		cubeindex |= 16
-	if grid.val[5] < isolevel:
-		cubeindex |= 32
-	if grid.val[6] < isolevel:
-		cubeindex |= 64
-	if grid.val[7] < isolevel:
-		cubeindex |= 128
+		# Cube is entirely in/out of the surface
+		if edgeTable.get_int(cubeindex) == 0:
+			continue
+			
+		# Find the vertices where the surface intersects the cube
+		if edgeTable.get_int(cubeindex) & 1:
+			vertlist[0] = VertexInterp(isolevel, gc_pos[i0], gc_pos[i1], gc_val[i0], gc_val[i1])
+			normlist[0] = VertexInterp(isolevel, gc_norm[i0], gc_norm[i1], gc_val[i0], gc_val[i1])
+		if edgeTable.get_int(cubeindex) & 2:
+			vertlist[1] = VertexInterp(isolevel, gc_pos[i1], gc_pos[i2], gc_val[i1], gc_val[i2])
+			normlist[1] = VertexInterp(isolevel, gc_norm[i1], gc_norm[i2], gc_val[i1], gc_val[i2])
+		if edgeTable.get_int(cubeindex) & 4:
+			vertlist[2] = VertexInterp(isolevel, gc_pos[i2], gc_pos[i3], gc_val[i2], gc_val[i3])
+			normlist[2] = VertexInterp(isolevel, gc_norm[i2], gc_norm[i3], gc_val[i2], gc_val[i3])
+		if edgeTable.get_int(cubeindex) & 8:
+			vertlist[3] = VertexInterp(isolevel, gc_pos[i3], gc_pos[i0], gc_val[i3], gc_val[i0])
+			normlist[3] = VertexInterp(isolevel, gc_norm[i3], gc_norm[i0], gc_val[i3], gc_val[i0])
+		if edgeTable.get_int(cubeindex) & 16:
+			vertlist[4] = VertexInterp(isolevel, gc_pos[i4], gc_pos[i5], gc_val[i4], gc_val[i5])
+			normlist[4] = VertexInterp(isolevel, gc_norm[i4], gc_norm[i5], gc_val[i4], gc_val[i5])
+		if edgeTable.get_int(cubeindex) & 32:
+			vertlist[5] = VertexInterp(isolevel, gc_pos[i5], gc_pos[i6], gc_val[i5], gc_val[i6])
+			normlist[5] = VertexInterp(isolevel, gc_norm[i5], gc_norm[i6], gc_val[i5], gc_val[i6])
+		if edgeTable.get_int(cubeindex) & 64:
+			vertlist[6] = VertexInterp(isolevel, gc_pos[i6], gc_pos[i7], gc_val[i6], gc_val[i7])
+			normlist[6] = VertexInterp(isolevel, gc_norm[i6], gc_norm[i7], gc_val[i6], gc_val[i7])
+		if edgeTable.get_int(cubeindex) & 128:
+			vertlist[7] = VertexInterp(isolevel, gc_pos[i7], gc_pos[i4], gc_val[i7], gc_val[i4])
+			normlist[7] = VertexInterp(isolevel, gc_norm[i7], gc_norm[i4], gc_val[i7], gc_val[i4])
+		if edgeTable.get_int(cubeindex) & 256:
+			vertlist[8] = VertexInterp(isolevel, gc_pos[i0], gc_pos[i4], gc_val[i0], gc_val[i4])
+			normlist[8] = VertexInterp(isolevel, gc_norm[i0], gc_norm[i4], gc_val[i0], gc_val[i4])
+		if edgeTable.get_int(cubeindex) & 512:
+			vertlist[9] = VertexInterp(isolevel, gc_pos[i1], gc_pos[i5], gc_val[i1], gc_val[i5])
+			normlist[9] = VertexInterp(isolevel, gc_norm[i1], gc_norm[i5], gc_val[i1], gc_val[i5])
+		if edgeTable.get_int(cubeindex) & 1024:
+			vertlist[10] = VertexInterp(isolevel, gc_pos[i2], gc_pos[i6], gc_val[i2], gc_val[i6])
+			normlist[10] = VertexInterp(isolevel, gc_norm[i2], gc_norm[i6], gc_val[i2], gc_val[i6])
+		if edgeTable.get_int(cubeindex) & 2048:
+			vertlist[11] = VertexInterp(isolevel, gc_pos[i3], gc_pos[i7], gc_val[i3], gc_val[i7])
+			normlist[11] = VertexInterp(isolevel, gc_norm[i3], gc_norm[i7], gc_val[i3], gc_val[i7])
 	
-	# Cube is entirely in/out of the surface
-	if lookup.edgeTables.get_int(cubeindex) == 0:
-		return 0
-	
-	# Find the vertices where the surface intersects the cube
-	if lookup.edgeTables.get_int(cubeindex) & 1:
-		vertlist[0] = VertexInterp(isolevel, grid.p[0], grid.p[1], grid.val[0], grid.val[1])
-		normlist[0] = VertexInterp(isolevel, grid.norm[0], grid.norm[1], grid.val[0], grid.val[1])
-	if lookup.edgeTables.get_int(cubeindex) & 2:
-		vertlist[1] = VertexInterp(isolevel, grid.p[1], grid.p[2], grid.val[1], grid.val[2])
-		normlist[1] = VertexInterp(isolevel, grid.norm[1], grid.norm[2], grid.val[1], grid.val[2])
-	if lookup.edgeTables.get_int(cubeindex) & 4:
-		vertlist[2] = VertexInterp(isolevel, grid.p[2], grid.p[3], grid.val[2], grid.val[3])
-		normlist[2] = VertexInterp(isolevel, grid.norm[2], grid.norm[3], grid.val[2], grid.val[3])
-	if lookup.edgeTables.get_int(cubeindex) & 8:
-		vertlist[3] = VertexInterp(isolevel, grid.p[3], grid.p[0], grid.val[3], grid.val[0])
-		normlist[3] = VertexInterp(isolevel, grid.norm[3], grid.norm[0], grid.val[3], grid.val[0])
-	if lookup.edgeTables.get_int(cubeindex) & 16:
-		vertlist[4] = VertexInterp(isolevel, grid.p[4], grid.p[5], grid.val[4], grid.val[5])
-		normlist[4] = VertexInterp(isolevel, grid.norm[4], grid.norm[5], grid.val[4], grid.val[5])
-	if lookup.edgeTables.get_int(cubeindex) & 32:
-		vertlist[5] = VertexInterp(isolevel, grid.p[5], grid.p[6], grid.val[5], grid.val[6])
-		normlist[5] = VertexInterp(isolevel, grid.norm[5], grid.norm[6], grid.val[5], grid.val[6])
-	if lookup.edgeTables.get_int(cubeindex) & 64:
-		vertlist[6] = VertexInterp(isolevel, grid.p[6], grid.p[7], grid.val[6], grid.val[7])
-		normlist[6] = VertexInterp(isolevel, grid.norm[6], grid.norm[7], grid.val[6], grid.val[7])
-	if lookup.edgeTables.get_int(cubeindex) & 128:
-		vertlist[7] = VertexInterp(isolevel, grid.p[7], grid.p[4], grid.val[7], grid.val[4])
-		normlist[7] = VertexInterp(isolevel, grid.norm[7], grid.norm[4], grid.val[7], grid.val[4])
-	if lookup.edgeTables.get_int(cubeindex) & 256:
-		vertlist[8] = VertexInterp(isolevel, grid.p[0], grid.p[4], grid.val[0], grid.val[4])
-		normlist[8] = VertexInterp(isolevel, grid.norm[0], grid.norm[4], grid.val[0], grid.val[4])
-	if lookup.edgeTables.get_int(cubeindex) & 512:
-		vertlist[9] = VertexInterp(isolevel, grid.p[1], grid.p[5], grid.val[1], grid.val[5])
-		normlist[9] = VertexInterp(isolevel, grid.norm[1], grid.norm[5], grid.val[1], grid.val[5])
-	if lookup.edgeTables.get_int(cubeindex) & 1024:
-		vertlist[10] = VertexInterp(isolevel, grid.p[2], grid.p[6], grid.val[2], grid.val[6])
-		normlist[10] = VertexInterp(isolevel, grid.norm[2], grid.norm[6], grid.val[2], grid.val[6])
-	if lookup.edgeTables.get_int(cubeindex) & 2048:
-		vertlist[11] = VertexInterp(isolevel, grid.p[3], grid.p[7], grid.val[3], grid.val[7])
-		normlist[11] = VertexInterp(isolevel, grid.norm[3], grid.norm[7], grid.val[3], grid.val[7])
-	
-	# Create the traingle
-	ntriang = 0
-	i = 0
-	triangles.clear()
-	triangles.resize(5)
-	while lookup.triTable.get_int(cubeindex, i) != -1:
-		triangles[ntriang] = TRIANGLE.new()
-		triangles[ntriang].p.resize(3)
-		triangles[ntriang].n.resize(3)
-		triangles[ntriang].p[0] = vertlist[lookup.triTable.get_int(cubeindex, i)]
-		triangles[ntriang].p[1] = vertlist[lookup.triTable.get_int(cubeindex, i+1)]
-		triangles[ntriang].p[2] = vertlist[lookup.triTable.get_int(cubeindex, i+2)]
-		triangles[ntriang].n[0] = normlist[lookup.triTable.get_int(cubeindex, i)]
-		triangles[ntriang].n[1] = normlist[lookup.triTable.get_int(cubeindex, i+1)]
-		triangles[ntriang].n[2] = normlist[lookup.triTable.get_int(cubeindex, i+2)]
-		i += 3
-		ntriang += 1
+		# Create the traingles
+		i = 0
+		while triTable.get_int(cubeindex, i) != -1:
+			tri_pos.append(vertlist[triTable.get_int(cubeindex, i)])
+			tri_pos.append(vertlist[triTable.get_int(cubeindex, i+1)])
+			tri_pos.append(vertlist[triTable.get_int(cubeindex, i+2)])
+			tri_norm.append(normlist[triTable.get_int(cubeindex, i)])
+			tri_norm.append(normlist[triTable.get_int(cubeindex, i+1)])
+			tri_norm.append(normlist[triTable.get_int(cubeindex, i+2)])
+			i += 3
+			ntriang += 1
 	
 	# Ultimately I want to processs and populate the elements inside the tri_p and tri_n
-	
 	return ntriang
